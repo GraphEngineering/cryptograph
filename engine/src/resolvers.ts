50703ede073fec09442207d1a735fa3bbd9d16f3
Conner Ruhl <connerruhl@me.com>
2018-01-14 22:13:36 -0600

    Broken code to update Todd

--- engine/src/resolvers.ts
+++ engine/src/resolvers.ts
@@ -1,10 +1,21 @@
 export default {
   Query: {
-    tokens: () => []
+    tokens: () => tokens
   },
-  Token: {
+  Token: () => ({
     name: () => null,
     website: () => null,
     markdown: () => null
-  }
+  })
 };
+
+const tokens = [
+  {
+    name: "The Request Network",
+    website: "https://request.network/",
+    markdown: `
+# Request Network
+A decentralized network for payment requests
+`
+  }
+];
