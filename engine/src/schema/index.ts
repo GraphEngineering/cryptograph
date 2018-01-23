0aad4c70565d647af57966172af13cdac73f353e
Conner Ruhl <connerruhl@me.com>
2018-01-22 18:49:42 -0600

    Working again, moving on to stricter resolver definitions

--- engine/src/schema/index.ts
+++ engine/src/schema/index.ts
@@ -1,32 +1,14 @@
 import {
   buildSchema,
-  GraphQLEnumType,
-  GraphQLEnumValue,
-  GraphQLField,
-  GraphQLFieldResolver,
   GraphQLInputType,
-  GraphQLLeafType,
-  GraphQLList,
-  GraphQLNonNull,
   GraphQLObjectType,
-  GraphQLOutputType,
   GraphQLScalarType,
   GraphQLSchema,
   isAbstractType,
-  isLeafType,
   valueFromAST
 } from "graphql";
 
-type Resolver<TSource> = (
-  type: TSource,
-  field?: GraphQLField<TSource, any>
-) => GraphQLFieldResolver<TSource, any>;
-
-type FieldResolver = Resolver<any>;
-type LeafResolver = Resolver<any>;
-type EnumResolver = Resolver<any>;
-type ScalarResolver = Resolver<any>;
-type PrimitiveResolver = Resolver<any>;
+import { fieldResolver, primitiveNames } from "./resolvers";
 
 export const sourceToSchema = (source: string): GraphQLSchema =>
   attachResolvers(buildSchema(source));
@@ -35,10 +17,7 @@ const attachResolvers = (schema: GraphQLSchema): GraphQLSchema => {
   Object.values(schema.getTypeMap()).forEach(type => {
     const { name } = type;
 
-    if (
-      type instanceof GraphQLScalarType &&
-      !primitiveTypeNames.includes(name)
-    ) {
+    if (type instanceof GraphQLScalarType && !primitiveNames.includes(name)) {
       type.serialize = type.parseValue = value => value;
       type.parseLiteral = valueNode =>
         valueFromAST(valueNode, type as GraphQLInputType);
@@ -57,47 +36,3 @@ const attachResolvers = (schema: GraphQLSchema): GraphQLSchema => {
 
   return schema;
 };
-
-const fieldResolver: FieldResolver = (type, field) => {
-  if (type instanceof GraphQLNonNull) {
-    return fieldResolver(type.ofType, field);
-  }
-
-  if (type instanceof GraphQLList) {
-    return (source, context, args, info) => [
-      fieldResolver(type.ofType, field)(source, context, args, info)
-    ];
-  }
-
-  if (isAbstractType(type)) {
-    return () => ({ __typename: type.name });
-  }
-
-  if (isLeafType(type)) {
-    return leafTypeResolver(type);
-  }
-
-  return () => ({});
-};
-
-const leafTypeResolver: LeafResolver = type =>
-  type instanceof GraphQLEnumType
-    ? enumResolver(type)
-    : scalarResolver(type) || (() => `<${type.name}>`);
-
-const enumResolver: EnumResolver = type => () => type.getValues()[0].value;
-
-const scalarResolver: ScalarResolver = type => primitiveResolver(type.name);
-
-const primitiveResolver: PrimitiveResolver = type =>
-  primitiveResolvers[type.name];
-
-const primitiveResolvers: { [name: string]: () => any } = {
-  Boolean: () => true,
-  Float: () => 4.2,
-  ID: () => "ID",
-  Int: () => 420,
-  String: () => "string"
-};
-
-const primitiveTypeNames = Object.keys(primitiveResolvers);
