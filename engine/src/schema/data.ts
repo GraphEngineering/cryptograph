fb8394e40299845b7242c5952254cb1bed9a3ce1
Aaron <aarondonoho@gmail.com>
2018-01-24 22:50:16 -0600

    Getting closer...?

---
+++ engine/src/schema/data.ts
@@ -0,0 +1,5 @@
+export const IpfsCid = {
+  name: 'IpfsCid',
+  description: 'The IPFS Cid represents content accessible on the IPFS.',
+  error: 'Query error: Not a valid IPFS Cid'
+};
--- engine/src/schema/resolvers.ts
+++ engine/src/schema/resolvers.ts
@@ -8,16 +8,21 @@ import {
   GraphQLEnumType
 } from "graphql";
 
+import  { IpfsCid } from "./customTypes"
+
 type Resolver<TSource> = (
   type: TSource,
   field?: GraphQLField<TSource, any>
 ) => GraphQLFieldResolver<TSource, any>;
 
+//change from any type to leafNodeResolver
+// replace any with GraphQLFieldType
 type FieldResolver = Resolver<any>;
 type LeafResolver = Resolver<any>;
 type EnumResolver = Resolver<any>;
 type ScalarResolver = Resolver<any>;
 type PrimitiveResolver = Resolver<any>;
+type IpfsResolver = Resolver<IpfsCid>;
 
 export const fieldResolver: FieldResolver = (type, field) => {
   if (type instanceof GraphQLNonNull) {
@@ -53,6 +58,8 @@ const scalarResolver: ScalarResolver = type => primitiveResolver(type);
 const primitiveResolver: PrimitiveResolver = type =>
   primitiveResolvers[type.name];
 
+const ipfsResolver: IpfsResolver = type => primitiveResolver(type);
+
 const primitiveResolvers: {
   [name: string]: () => boolean | number | string;
 } = {
@@ -60,7 +67,8 @@ const primitiveResolvers: {
   Float: () => 12.34,
   ID: () => "ID",
   Int: () => 1234,
-  String: () => "Hello, World!"
+  String: () => "Hello, World!",
+  IpfsCid: () => "QmW2WQi7j6c7UgJTarActp7tDNikE4B2qXtFCfLPdsgaTQ/cat.jpg"
 };
 
 export const primitiveNames = Object.keys(primitiveResolvers);
