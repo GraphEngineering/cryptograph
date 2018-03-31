50f8f124d2ea238c730edfe52f54dbfc1546d910
Conner Ruhl <connerruhl@me.com>
2018-03-31 12:23:56 -0500

    Basic server set up complete

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-export const graph = {};
+import express from "express";
+import * as bodyParser from "body-parser";
+
+import { makeExecutableSchema } from "graphql-tools";
+import { graphqlExpress } from "apollo-server-express";
+
+import expressPlayground from "graphql-playground-middleware-express";
+
+const schema = makeExecutableSchema({
+  typeDefs: `
+    type Query {
+      hello: String!
+    }
+  `,
+  resolvers: {
+    Query: {
+      hello: () => "world"
+    }
+  }
+});
+
+const PORT = 4000;
+
+const app = express();
+
+app.use("/graph", bodyParser.json(), graphqlExpress({ schema }));
+app.get("/playground", expressPlayground({ endpoint: "/graph" }));
+
+app.listen(4000);
+
+console.log(` http://localhost:${PORT}/playground`);
